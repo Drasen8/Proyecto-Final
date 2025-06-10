c3520f9d3afd0357427f1ae752a5376cd5a6afe0
Drasen8 <sendra.marc5256@gmail.com>
2025-06-10 17:50:11 -0600

    base de datos railway

--- models/db.js
+++ models/db.js
@@ -1,12 +1,7 @@
 
 const dbConfig = require("../config/db.config.js");
 const mysql = require("mysql2")// Create a connection to the database
-const connection = mysql.createConnection({
- host: dbConfig.HOST,
- user: dbConfig.USER,
- password: dbConfig.PASSWORD,
- database: dbConfig.DB
-});
+const connection = mysql.createConnection(process.env.MYSQL_URL);
 // open the MySQL connection
 connection.connect((err) => {
     if (err) {
