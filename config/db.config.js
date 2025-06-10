3600a939e635f3a37d1e62c3587cce4d6604667d
Drasen8 <sendra.marc5256@gmail.com>
2025-06-10 17:33:15 -0600

    config: update db config for local development

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    DB: "proyecto",
-    USER: "root",
-    PASSWORD:"admin4848"
+    HOST: process.env.MYSQLHOST || "localhost",
+    DB: process.env.MYSQLUSER || "proyecto",
+    USER: process.env.MYSQLPASSWORD || "root",
+    PASSWORD: process.env.MYSQLHOST || "admin4848",
+    PORT: process.env.MYSQLPORT || 3306,
 };
