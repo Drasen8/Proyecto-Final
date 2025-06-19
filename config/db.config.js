d3ea9f8afcaf5db13eff369227a991a835f2f1fc
Drasen8 <sendra.marc5256@gmail.com>
2025-06-19 07:58:26 -0600

    Ultimos cambios

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: process.env.MYSQLHOST || "localhost",
-    DB: process.env.MYSQLUSER || "proyecto",
-    USER: process.env.MYSQLPASSWORD || "root",
-    PASSWORD: process.env.MYSQLHOST || "admin4848",
+    HOST: process.env.MYSQLHOST || "mysql.railway.internal",
+    DB: process.env.MYSQLUSER || "root",
+    USER: process.env.MYSQLUSER || "root",
+    PASSWORD: process.env.MYSQLPASSWORD || "admin4848",
     PORT: process.env.MYSQLPORT || 3306,
 };
